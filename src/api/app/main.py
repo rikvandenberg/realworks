2033e5ea30215fdd6e6b97dd75b270735130912a
Rik van den Berg <rikvandenberg@users.noreply.github.com>
2023-10-12 13:05:38 +0200

    Completed rolling update and resolve env variables not working

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -4,6 +4,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/config/")
+@app.get("/configs")
+def read_configs():
+    response = {}
+    for name, value in os.environ.items():
+        response[name] = value
+    return response
+    
+
+
+@app.get("/configs/message")
 def read_config():
     return {"message": os.getenv('MESSAGE')} 
+
